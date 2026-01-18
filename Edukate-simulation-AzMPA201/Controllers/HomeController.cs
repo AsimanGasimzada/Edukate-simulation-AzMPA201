2a33011e32724c2a8334d320ce5893b32beaac0f
AsimanGasimzada <asiman.qasimzade@gmail.com>
2026-01-18 13:28:15 +0400

    Add project files.

---
+++ Edukate-simulation-AzMPA201/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Edukate_simulation_AzMPA201.Controllers;
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Edukate-simulation-AzMPA201/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Edukate_simulation_AzMPA201.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
